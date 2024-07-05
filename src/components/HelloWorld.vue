2509a052ceb2cac9dd93b97010b63427989887ff
Damien Courouble <damien.courouble@clicdata.com>
2024-07-05 17:11:35 +0200

    work

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,60 +5,71 @@
 </template>
 
 <script>
-import * as monaco from "monaco-editor";
 import JSZip from "jszip";
-import * as vscode from "vscode";
-import { initServices } from "monaco-languageclient/vscode/services";
 import getThemeServiceOverride from "@codingame/monaco-vscode-theme-service-override";
 
-import getConfigurationServiceOverride, {
-  updateUserConfiguration,
-} from "@codingame/monaco-vscode-configuration-service-override";
+import getConfigurationServiceOverride from "@codingame/monaco-vscode-configuration-service-override";
 import getTextmateServiceOverride from "@codingame/monaco-vscode-textmate-service-override";
 import "@codingame/monaco-vscode-theme-defaults-default-extension";
-import { whenReady } from "@codingame/monaco-vscode-python-default-extension";
-import {
-  RegisteredFileSystemProvider,
-  registerFileSystemOverlay,
-  RegisteredMemoryFile,
-} from "@codingame/monaco-vscode-files-service-override";
-import { MonacoLanguageClient } from "monaco-languageclient";
-import { CloseAction, ErrorAction } from "vscode-languageclient";
-import { useWorkerFactory } from "monaco-editor-wrapper/workerFactory";
 
+import { CloseAction, ErrorAction } from "vscode-languageclient";
+import "@codingame/monaco-vscode-python-default-extension";
+import { useWorkerFactory } from "monaco-editor-wrapper/workerFactory";
+import { MonacoEditorLanguageClientWrapper } from "monaco-editor-wrapper";
 import {
   BrowserMessageReader,
   BrowserMessageWriter,
 } from "vscode-languageserver-protocol/browser.js";
 import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-//import PyrightWorker from "@typefox/pyright-browser/dist/pyright.worker.js?worker";
-
-//import { Uri } from "vscode";
 
 export default {
   name: "MonacoEditor",
   data() {
-    return {
-      editor: null,
-      //pyrightWorker: null,
-    };
+    return {};
   },
   async mounted() {
-    useWorkerFactory({
-      ignoreMapping: true,
-      workerLoaders: {
-        editorWorkerService: () => new EditorWorker(),
-      },
-    });
-
     this.files = await this.readZipFile(
       new URL("./stdlib-source-with-typeshed-pyi.zip", window.location.href)
         .href
     );
 
-    debugger;
+    const pythonWorkerUrl = new URL(
+      "../../node_modules/@typefox/pyright-browser/dist/pyright.worker.js",
+      window.location.href
+    ).href;
 
-    await initServices({
+    const pyrightWorker = new Worker(pythonWorkerUrl);
+    const transports = {
+      reader: new BrowserMessageReader(pyrightWorker),
+      writer: new BrowserMessageWriter(pyrightWorker),
+    };
+    // Launch pyright language server
+    pyrightWorker.postMessage({
+      type: "browser/boot",
+      mode: "foreground",
+    });
+
+    // ------------[ Monaco Editor Wrapper config ]------------
+    const wrapperConfig = {
+      editorAppConfig: {
+        $type: "classic",
+        useDiffEditor: false,
+        codeResources: {
+          main: {
+            text: 'def test():\n  print("Hello Python")',
+            fileExt: "py",
+            enforceLanguageId: "python",
+          },
+        },
+        editorOptions: {
+          language: "python",
+          //language: "javascript",
+          minimap: {
+            enabled: true,
+          },
+          theme: "vs-dark",
+        },
+      },
       serviceConfig: {
         userServices: {
           ...getThemeServiceOverride(),
@@ -67,59 +78,112 @@ export default {
         },
         debugLogging: true,
       },
-    });
-
-    await whenReady();
-
-    monaco.editor.create(document.getElementById("monaco-editor-root"), {
-      autoIndent: "full",
-      fixedOverflowWidgets: true,
-      value: `print("Hello World")`,
-      automaticLayout: true,
-      language: "python",
-      theme: "vs-dark",
-      minimap: {
-        enabled: false,
+    };
+    const languageClientConfig = {
+      name: "Pyright Language Client",
+      languageId: "python",
+      options: {
+        $type: "WorkerDirect",
+        worker: pyrightWorker,
       },
-      wordBasedSuggestions: "off",
-      wordWrap: "on",
+      clientOptions: {
+        documentSelector: ["python"],
+        errorHandler: {
+          error: () => ({ action: ErrorAction.Continue }),
+          closed: () => ({ action: CloseAction.DoNotRestart }),
+        },
+        initializationOptions: {
+          files: this.files,
+        },
+      },
+      connectionProvider: {
+        get: () => Promise.resolve(transports),
+      },
+    };
+    const loggerConfig = {
+      enabled: true,
+      debugEnabled: true,
+    };
+
+    useWorkerFactory({
+      ignoreMapping: true,
+      workerLoaders: {
+        editorWorkerService: () => new EditorWorker(),
+      },
     });
 
-    updateUserConfiguration(`{
-      "workbench.colorCustomizations": {
-            "editor.background": "#fff",
-        }
-    }`);
+    const wrapper = new MonacoEditorLanguageClientWrapper();
+    const userConfig = { wrapperConfig, languageClientConfig, loggerConfig };
 
-    console.log(vscode.Uri.file("/workspace/hello.py"));
+    await wrapper.initAndStart(
+      userConfig,
+      document.getElementById("monaco-editor-root")
+    );
 
-    // const fileSystemProvider = new RegisteredFileSystemProvider(false);
-    // fileSystemProvider.registerFile(
-    //   new RegisteredMemoryFile(
-    //     vscode.Uri.file("/workspace/hello.py"),
-    //     'print("Hellosssss, World!")'
-    //   )
-    // );
-    // registerFileSystemOverlay(1, fileSystemProvider);
+    // debugger;
 
-    const pythonWorkerUrl = new URL(
-      "../../node_modules/@typefox/pyright-browser/dist/pyright.worker.js",
-      window.location.href
-    ).href;
-    console.info(`main.ts, pythonWorkerUrl: ${pythonWorkerUrl}`);
-    const worker = new Worker(pythonWorkerUrl);
+    // await initServices({
+    //   serviceConfig: {
+    //     userServices: {
+    //       ...getThemeServiceOverride(),
+    //       ...getTextmateServiceOverride(),
+    //       ...getConfigurationServiceOverride(),
+    //     },
+    //     debugLogging: true,
+    //   },
+    // });
 
-    //const worker = new PyrightWorker();
+    // await whenReady();
 
-    worker.postMessage({
-      type: "browser/boot",
-      mode: "foreground",
-    });
-    const reader = new BrowserMessageReader(worker);
-    const writer = new BrowserMessageWriter(worker);
-    const languageClient = this.createLanguageClient({ reader, writer });
-    languageClient.start();
-    reader.onClose(() => languageClient.stop());
+    // monaco.editor.create(document.getElementById("monaco-editor-root"), {
+    //   autoIndent: "full",
+    //   fixedOverflowWidgets: true,
+    //   value: `print("Hello World")`,
+    //   automaticLayout: true,
+    //   language: "python",
+    //   theme: "vs-dark",
+    //   minimap: {
+    //     enabled: false,
+    //   },
+    //   wordBasedSuggestions: "off",
+    //   wordWrap: "on",
+    // });
+
+    // updateUserConfiguration(`{
+    //   "workbench.colorCustomizations": {
+    //         "editor.background": "#fff",
+    //     }
+    // }`);
+
+    // console.log(vscode.Uri.file("/workspace/hello.py"));
+
+    // // const fileSystemProvider = new RegisteredFileSystemProvider(false);
+    // // fileSystemProvider.registerFile(
+    // //   new RegisteredMemoryFile(
+    // //     vscode.Uri.file("/workspace/hello.py"),
+    // //     'print("Hellosssss, World!")'
+    // //   )
+    // // );
+    // // registerFileSystemOverlay(1, fileSystemProvider);
+
+    // const pythonWorkerUrl = new URL(
+    //   "../../node_modules/@typefox/pyright-browser/dist/pyright.worker.js",
+    //   window.location.href
+    // ).href;
+    // console.info(`main.ts, pythonWorkerUrl: ${pythonWorkerUrl}`);
+    // const worker = new Worker(pythonWorkerUrl);
+
+    // //const worker = new PyrightWorker();
+
+    // worker.postMessage({
+    //   type: "browser/boot",
+    //   mode: "foreground",
+    // });
+    // const reader = new BrowserMessageReader(worker);
+    // const writer = new BrowserMessageWriter(worker);
+    // const languageClient = this.createLanguageClient({ reader, writer });
+    // languageClient.start();
+    // reader.onClose(() => languageClient.stop());
   },
 
   methods: {
@@ -142,34 +206,34 @@ export default {
         return {};
       }
     },
-    createLanguageClient(transports) {
-      return new MonacoLanguageClient({
-        name: "Pyright Language Client",
-        clientOptions: {
-          documentSelector: ["python"],
-          errorHandler: {
-            error: () => ({ action: ErrorAction.Continue }),
-            closed: () => ({ action: CloseAction.DoNotRestart }),
-          },
-          // workspaceFolder: {
-          //   index: 0,
-          //   name: "/workspace",
-          //   uri: vscode.Uri.file("/workspace"),
-          // },
-          // synchronize: {
-          //   fileEvents: [vscode.workspace.createFileSystemWatcher("**")],
-          // },
-          initializationOptions: {
-            files: this.files,
-          },
-        },
-        connectionProvider: {
-          get: () => {
-            return Promise.resolve(transports);
-          },
-        },
-      });
-    },
+    // createLanguageClient(transports) {
+    //   return new MonacoLanguageClient({
+    //     name: "Pyright Language Client",
+    //     clientOptions: {
+    //       documentSelector: ["python"],
+    //       errorHandler: {
+    //         error: () => ({ action: ErrorAction.Continue }),
+    //         closed: () => ({ action: CloseAction.DoNotRestart }),
+    //       },
+    //       // workspaceFolder: {
+    //       //   index: 0,
+    //       //   name: "/workspace",
+    //       //   uri: vscode.Uri.file("/workspace"),
+    //       // },
+    //       // synchronize: {
+    //       //   fileEvents: [vscode.workspace.createFileSystemWatcher("**")],
+    //       // },
+    //       initializationOptions: {
+    //         files: this.files,
+    //       },
+    //     },
+    //     connectionProvider: {
+    //       get: () => {
+    //         return Promise.resolve(transports);
+    //       },
+    //     },
+    //   });
+    // },
   },
 };
 </script>
